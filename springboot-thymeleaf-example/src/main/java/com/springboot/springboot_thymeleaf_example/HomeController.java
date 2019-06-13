c94a5b0d5a2d6c975d2603a13ae15df4d4f08b20
vino_eie07 <vino_eie07@Vinod>
2019-06-13 11:12:22 +0530

    Commit 3

--- springboot-thymeleaf-example/src/main/java/com/springboot/springboot_thymeleaf_example/HomeController.java
+++ springboot-thymeleaf-example/src/main/java/com/springboot/springboot_thymeleaf_example/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 public class HomeController {
 	@RequestMapping("/")
 	public String index() {
+		System.out.println("Index mapping");
 		return "index";
 	}
 
