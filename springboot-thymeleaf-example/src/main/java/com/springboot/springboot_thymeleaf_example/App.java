62d46ff1da9e9bc96645fefc6b8bc4baaaf0dbec
vino_eie07 <vino_eie07@Vinod>
2019-06-15 14:33:50 +0530

    App change

--- springboot-thymeleaf-example/src/main/java/com/springboot/springboot_thymeleaf_example/App.java
+++ springboot-thymeleaf-example/src/main/java/com/springboot/springboot_thymeleaf_example/App.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App 
 {
 	  public static void main(String[] args) {  
+		  System.out.println("Hello World App");
 	        SpringApplication.run(App.class, args);  
 	    }  
 }
