53e62a6c7df869523c359a05f9a73d43392a5212
vino_eie07 <vinodoller@gmail.com>
2019-06-15 21:45:17 +0530

    Inner user class latest

--- springboot-thymeleaf-example/src/main/java/com/springboot/springboot_thymeleaf_example/HomeController.java
+++ springboot-thymeleaf-example/src/main/java/com/springboot/springboot_thymeleaf_example/HomeController.java
@@ -1,5 +1,6 @@
 package com.springboot.springboot_thymeleaf_example;
 
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,11 +16,33 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public ModelAndView save(@ModelAttribute User user) {
+	public ModelAndView save(@ModelAttribute HomeController user) {
 		ModelAndView modelAndView = new ModelAndView();
 		System.out.println("Home Controller");
 		modelAndView.setViewName("user-data");
 		modelAndView.addObject("user", user);
 		return modelAndView;
+	}
+}
+
+class User {
+
+	String name;
+	String email;
+
+	public String getName() {  
+        return name;  
+    }
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {  
+        return email;  
+    }
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
